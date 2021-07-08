a635960a184a4af39508539bc4adb772c667d1c7
matesa <75279583+matesa@users.noreply.github.com>
2021-07-08 09:28:31 +0300

    Create str.py

---
+++ str.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from pyrogram import Client
+
+print("my.telegram.org/apps adresinden uygulama bilgilerinizi aşağıya girin.")
+
+
+async def main():
+    async with Client(
+        ":memory:", api_id=int(input("API ID:")), api_hash=input("API HASH:")
+    ) as app:
+        print(await app.export_session_string())
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
